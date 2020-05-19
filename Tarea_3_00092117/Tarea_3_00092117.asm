548bfc6f4635bb9b5347479b7d9c76603d37769b
Eduardo <00092117@uca.edu.sv>
2020-05-18 19:26:35 -0600

    Tarea 3

--- Tarea_3_00092117/Tarea_3_00092117.asm
+++ Tarea_3_00092117/Tarea_3_00092117.asm
@@ -5,6 +5,9 @@ section .text
 	call 	texto	
 	call 	cursor
 	call 	phrase
+	call 	phrase2
+	call 	phrase3
+	call 	phrase4
 	call	kbwait
 
 	int 	20h
@@ -35,8 +38,32 @@ kbwait: mov 	ax, 0000h
 
 m_cursr:mov 	ah, 02h
 	mov 	dx, di  ; columna
-	ADD		dl, 29d ;Añadir el offset de la columna
-	mov 	dh, 12d ; fila
+	ADD	dl, 17d ;Añadir el offset de la columna
+	mov 	dh, 5d ; fila
+	mov 	bh, 0h
+	int 	10h
+	ret
+
+m_cursr2:mov 	ah, 02h
+	mov 	dx, di  ; columna
+	ADD	dl, 05d ;Añadir el offset de la columna
+	mov 	dh, 10d ; fila
+	mov 	bh, 0h
+	int 	10h
+	ret
+
+m_cursr3:mov 	ah, 02h
+	mov 	dx, di  ; columna
+	ADD	dl, 29d ;Añadir el offset de la columna
+	mov 	dh, 15d ; fila
+	mov 	bh, 0h
+	int 	10h
+	ret
+
+m_cursr4:mov 	ah, 02h
+	mov 	dx, di  ; columna
+	ADD	dl, 05d ;Añadir el offset de la columna
+	mov 	dh, 18d ; fila
 	mov 	bh, 0h
 	int 	10h
 	ret
@@ -50,7 +77,43 @@ lupi:	mov 	cl, [msg+di]
 	jb	lupi
 	ret
 
+phrase2:	mov 	di, 0d
+lupi2:	mov 	cl, [msg2+di]
+	call    m_cursr2
+	call 	w_char
+	inc	di
+	cmp 	di, len3
+	jb	lupi2
+	ret
+
+phrase3:	mov di, 0d
+lupi3:	mov 	cl, [msg3+di]
+	call    m_cursr3
+	call 	w_char
+	inc	di
+	cmp 	di, len4
+	jb	lupi3
+	ret
+
+phrase4:	mov di, 0d
+lupi4:	mov 	cl, [msg4+di]
+	call    m_cursr4
+	call 	w_char
+	inc	di
+	cmp 	di, len2
+	jb	lupi4
+	ret
+
 
 section .data
-msg	db 	"Mami que tu quiere? "
+msg	db 	"Cada vez que pienso en ti,"
 len 	equ	$-msg
+
+msg2	db 	"mis ojos rompen en llanto"
+len3 	equ	$-msg2
+
+msg3	db 	"y muy triste me pregunto,"
+len4 	equ	$-msg3
+
+msg4	db 	"¿por que te quiero tanto?"
+len2 	equ	$-msg4
